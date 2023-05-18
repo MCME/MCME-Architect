9e74b6ccfa1636bdd8a8f09caf54c079883876e0
EriolEandur <Eriol_Eandur@gmx.de>
2023-05-18 16:23:36 +0200

    downgrade to 1.18

--- src/main/java/com/mcmiddleearth/architect/specialBlockHandling/data/ItemBlockData.java
+++ src/main/java/com/mcmiddleearth/architect/specialBlockHandling/data/ItemBlockData.java
@@ -18,16 +18,16 @@ package com.mcmiddleearth.architect.specialBlockHandling.data;
 
 import com.mcmiddleearth.architect.specialBlockHandling.specialBlocks.SpecialBlock;
 import com.mcmiddleearth.architect.specialBlockHandling.specialBlocks.SpecialBlockItemBlock;
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundGroup;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockSupport;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -211,23 +211,4 @@ Logger.getGlobal().info("****************");*/
         };
     }
 
-    @Override
-    public boolean isSupported(@NotNull Block block) {
-        return false;
-    }
-
-    @Override
-    public boolean isSupported(@NotNull Location location) {
-        return false;
-    }
-
-    @Override
-    public boolean isFaceSturdy(@NotNull BlockFace blockFace, @NotNull BlockSupport blockSupport) {
-        return false;
-    }
-
-    @Override
-    public boolean isRandomlyTicked() {
-        return false;
-    }
 }
